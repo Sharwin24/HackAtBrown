9a268952cfd10de6e0645d0385da493ac9a2e891
CharlesJoseph2003 <105239968+CharlesJoseph2003@users.noreply.github.com>
2024-03-08 17:23:11 -0500

    This commit adds a visualization feature to the visualization.py It extracts the components from the dictionary and builds a treemap

--- visualization.py
+++ visualization.py
@@ -1,55 +1,67 @@
 import plotly.express as px
 from napkin_graph import *
 
-codebase = CodeBase()
-codegraph = CodeGraph()
-visual_cg = VisualCodeGraph(codegraph)
-graph_dict = visual_cg.get_graph_dict()
-
-# graph_dict = {
-#     'autoregressive.py': ['__call__', 'one_step_prediction', 'loss', 'one_step_loss', '_get_flat_arrays_and_single_timestep_treedef', 'Predictor', '__init__', '_get_and_validate_constant_inputs', '_validate_targets_and_forcings'],
-#     '_get_flat_arrays_and_single_timestep_treedef': ['autoregressive.py', '__call__', 'loss'],
-#     '__init__': ['autoregressive.py', '__call__', 'loss', 'one_step_prediction', 'one_step_loss', 'Predictor', '_get_and_validate_constant_inputs', '_validate_targets_and_forcings']
-# }
-
-# print(graph_dict.values())
-parent = []
-children = []
-for key, value in graph_dict.items():
-    parent_key = (" " + key) * len(value)
-    parent.append(parent_key)
-    children.extend(value)
-print(parent)
-print(children)
-    # print("Key:", key, "Value:", value)
-
-
-fig = px.treemap(
-    names=['autoregressive.py', 'one_step_prediction', 'loss', 'one_step_loss', '_get_flat_arrays_and_single_timestep_treedef', 'Predictor', '__init__', '_get_and_validate_constant_inputs', '_validate_targets_and_forcings'],
-    parents=["", 'autoregressive.py', 'autoregressive.py', 'autoregressive.py', 'autoregressive.py', 'autoregressive.py', 'autoregressive.py', 'autoregressive.py', 'autoregressive.py']
-)
-fig.update_traces(root_color="lightgrey")
-fig.update_layout(margin=dict(t=50, l=25, r=25, b=25))
-# fig.show()
+# codebase = CodeBase()
+# codegraph = CodeGraph()
+# visual_cg = VisualCodeGraph(codegraph)
+# graph_dict = visual_cg.get_graph_dict()
 
 
 
-# fig = px.treemap(
-#     names = ["autoregressive.py","__call__", "one_step_prediction", "loss", "one_step_loss", "_get_flat_arrays_and_single_timestep_treedef", "Predictor", "__init__", 
-#              "_get_and_validate_constant_inputs", '_get_flat_arrays_and_single_timestep_treedef', 'Predictor', '__call__'],
+#This is an example graph_dict for testing puproses, replace with visual_cg.get_graph_dict()
+#Make sure that there are no repeated names for files, classes, and functions
+graph_dict= {
+    'file1.py': [{'Class1': ['Function1', 'Function2']}, {'Class2': ['Function3', 'Function4']}],
+    'file2.py': [{'Class2.1': ['Function5', 'Function6']}, {'Class2.2': ['Function7', 'Function8']}],
+    'file3.py': [{'Class3.1': ['Function9', 'Function10']}, {'Class3.2': ['Function11', 'Function12']}]
+}
 
-#     parents = ['', "autoregressive.py", "autoregressive.py", "autoregressive.py", "autoregressive.py", "autoregressive.py", "autoregressive.py", "autoregressive.py", 
-#                "autoregressive.py", '', '_get_flat_arrays_and_single_timestep_treedef',  '_get_flat_arrays_and_single_timestep_treedef']
-# )
-# fig.update_traces(root_color="lightgrey")
-# fig.update_layout(margin = dict(t=50, l=25, r=25, b=25))
-# # fig.show()
+class Visualization_Graph():
+    
+    graph_dict = graph_dict
+
+    def __init__(self) -> None:
+        self.names_list = []
+        self.parents_list = []
+
+    def generate_lists(self):
+        ''' Populate the names_list and parents_list attributes based on the graph_dict.
+            Iterates through the graph_dict, extracting file names, class names, and function names
+            to populate names_list and their corresponding parent relationships in parents_list.
+        '''
+        for file, classes in self.graph_dict.items():
+            self.names_list.append(file)
+            self.parents_list.append('')
+            for classes_dict in classes:
+                for class_name, functions in classes_dict.items():
+                    self.names_list.append(class_name)
+                    self.parents_list.append(file)
+                    # print(class_name)
+                    for function in functions:
+                        self.names_list.append(function)
+                        self.parents_list.append(class_name)
+                        # print(function)
+                        pass
 
 
-# fig = px.treemap(
-#     names = ["Eve","Cain", "Luke","Seth", "Enos", 'Mark', 'Jonah', "Noam", "Abel", "Awan", "Enoch", "Azura", 'John', 'Matthew', 'John', 'Enoch'],
-#     parents = ["", "Eve", "Cain","Eve", "Seth", 'Seth' , "Enos","Seth", "Eve", "Eve", "Awan", "Eve", 'Azura', 'Azura', 'Mark', 'Mark', 'Abel']
-# )
-# fig.update_traces(root_color="lightgrey")
-# fig.update_layout(margin = dict(t=50, l=25, r=25, b=25))
-# # fig.show()
+    def build_graph(self):
+        '''
+        Builds the treemap using the names_list and parents_list
+        '''
+        fig = px.treemap(
+
+            names= self.names_list,
+            parents=self.parents_list
+        )
+        fig.update_traces(root_color="lightgrey")
+        fig.update_layout(margin=dict(t=50, l=25, r=25, b=25))
+        fig.show()
+
+visualization_graph = Visualization_Graph()
+visualization_graph.generate_lists()
+visualization_graph.build_graph()
+# print(visualization_graph.names_list)
+# print(visualization_graph.parents_list)
+
+
+        
