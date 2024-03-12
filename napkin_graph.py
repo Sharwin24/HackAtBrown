4d8ea6c642a01b795ca657c2c900f3102f0a205d
Sharwin Patil <sharwin24@gmail.com>
2024-03-12 16:35:09 -0400

    Fixed NoneType bug for functions

--- napkin_graph.py
+++ napkin_graph.py
@@ -8,54 +8,56 @@ import ast
 import itertools
 import json
 
+
 class Component():
-	""" The base class for all components of the codebase
-	"""
-  
-	id_iter = itertools.count()
+    """ The base class for all components of the codebase
+    """
 
-	def __init__(self, name: str, parent: 'Component', children: 'list[Component]', raw: str) -> None:
-		self.name = name
-		self.parent = parent
-		self.children = children
-		self.raw = raw
-		self.id = next(self.id_iter)
-  
-	def get_type(self) -> str:
-		""" Get the type of the component [File, Class, Function]
-		"""
-		return self.__class__.__name__
+    id_iter = itertools.count()
 
-	def is_file(self) -> bool:
-		""" Check if the component is a file
-		"""
-		return self.get_type() == "File"
+    def __init__(self, name: str, parent: 'Component', children: 'list[Component]', raw: str) -> None:
+        self.name = name
+        self.parent = parent
+        self.children = children
+        self.raw = raw
+        self.id = next(self.id_iter)
 
-	def is_class(self) -> bool:
-		""" Check if the component is a class
-		"""
-		return self.get_type() == "Class"
+    def get_type(self) -> str:
+        """ Get the type of the component [File, Class, Function]
+        """
+        return self.__class__.__name__
 
-	def is_function(self) -> bool:
-		""" Check if the component is a function
-		"""
-		return self.get_type() == "Function"
+    def is_file(self) -> bool:
+        """ Check if the component is a file
+        """
+        return self.get_type() == "File"
 
-	def __repr__(self) -> str:
-		return f"{self.name}"
+    def is_class(self) -> bool:
+        """ Check if the component is a class
+        """
+        return self.get_type() == "Class"
 
-	def __hash__(self) -> int:
-		return hash((self.id))
+    def is_function(self) -> bool:
+        """ Check if the component is a function
+        """
+        return self.get_type() == "Function"
 
-	def __eq__(self, other: 'Component') -> bool:
-		if not isinstance(other, Component):
-			return False
-		return self.id == other.id
+    def __repr__(self) -> str:
+        return f"{self.name}"
+
+    def __hash__(self) -> int:
+        return hash((self.id))
+
+    def __eq__(self, other: 'Component') -> bool:
+        if not isinstance(other, Component):
+            return False
+        return self.id == other.id
+
 
 class File(Component):
     """ The class for a file in the codebase
     """
- 
+
     def __init__(self, name: str, parent: Component, children: 'list[Component]', raw: str, path: str, dependencies: 'list[File]') -> None:
         super().__init__(name, parent, children, raw)
         self.path = path
@@ -65,18 +67,21 @@ class File(Component):
         except UnicodeDecodeError:
             pass
 
+
 class Class(Component):
     """ The class for a class in the codebase
     """
     pass
 
+
 class Function(Component):
     pass
+
 
 class ComponentEdge:
     """ The class for an edge between two components in the codebase
     """
- 
+
     def __init__(self, from_component: Component, to_component: Component) -> None:
         self.from_component = from_component
         self.to_component = to_component
@@ -92,13 +97,16 @@ class ComponentEdge:
             return False
         return self.from_component == __value.from_component and self.to_component == __value.to_component
 
+
 class CodeBase:
     """ CodeBase is a representation of the codebase of a project, containing all files and their dependencies
     """
+
     def __init__(self, name: str, codebase_directory: str, repoLink: str = "", skipCloning: bool = False) -> None:
         self.name = name
-        self.fileDictionary = {} # dict[str, File] - file name to file object
-        self.funcDictionary = {} # dict[str, Function] - function name to function object
+        self.fileDictionary = {}  # dict[str, File] - file name to file object
+        # dict[str, Function] - function name to function object
+        self.funcDictionary = {}
         if repoLink != "" and not skipCloning:
             self.clone_repository(repoLink, codebase_directory)
         # Add all files in the subdirectories of the codebase to the list of files
@@ -106,10 +114,11 @@ class CodeBase:
             for file in files:
                 if file.endswith('.py'):
                     # Construct a Component object for the file
-                    f = File(name=file, path=os.path.join(root, file), parent=None, children=[], raw="", dependencies=[])
+                    f = File(name=file, path=os.path.join(root, file),
+                             parent=None, children=[], raw="", dependencies=[])
                     self.fileDictionary[file] = f
-        self.dependencies = {} # dict[file, list[dependency]]
-    
+        self.dependencies = {}  # dict[file, list[dependency]]
+
     def get_files(self) -> 'list[File]':
         """ Get a list of files in the codebase
 
@@ -128,7 +137,7 @@ class CodeBase:
             return self.dependencies[file]
         except KeyError:
             return []
- 
+
     def clone_repository(self, link: str, cloneDir: str = "example_codebase") -> None:
         """ Clones a repository from a link
 
@@ -146,7 +155,7 @@ class CodeBase:
         os.system("rm -rf " + cloneDir + "/.git")
         os.system("rm -rf " + cloneDir + "/.gitignore")
         print(f"Cloned repository from {link} to {cloneDir}")
-    
+
     def __repr__(self) -> str:
         """ Prints the CodeBase with files and their dependencies
 
@@ -161,18 +170,21 @@ class CodeBase:
                 value += f"{file}\n"
         return value
 
+
 class CodeGraph:
     """ The graph for a codebase with nodes representing components and edges representing the dependencies/imports
             Pass a complete CodeBase object to the constructor
     """
+
     def __init__(self, codebase: CodeBase) -> None:
         self.codebase = codebase
         files = codebase.get_files()
         if not files or len(files) == 0:
             raise ValueError("The codebase is empty")
-        self.nodes = [] # List[Component]
-        self.edges = [] # List[ComponentEdge]
-  
+        self.nodes = []  # List[Component]
+        self.edges = []  # List[ComponentEdge]
+        self.debug = False
+
     def __repr__(self) -> str:
         """ Prints the graph with nodes and edges
         """
@@ -181,6 +193,12 @@ class CodeGraph:
         for node in self.nodes:
             value += f"\t\t{node} -> {self.find_connected_nodes(node)}\n"
         return value
+
+    def set_debug(self, debug: bool) -> None:
+        """ Sets the debug mode for the graph,
+            Debug mode prints additional information for each operation
+        """
+        self.debug = debug
 
     def get_node_by_id(self, id: int) -> Component:
         """ Gets a node by its id
@@ -199,17 +217,17 @@ class CodeGraph:
             print(node.raw)
         else:
             print("Node not found")
-    
+
     def add_node(self, node: Component) -> None:
         """ Adds a node to the graph
         """
         self.nodes.append(node)
-    
+
     def add_edge(self, edge: ComponentEdge) -> None:
         """ Adds an edge to the graph
         """
         self.edges.append(edge)
-  
+
     def create_adjacency_matrix(self) -> 'list[list[int]]':
         """ Creates an adjacency matrix for the graph using the node ids
         """
@@ -221,9 +239,10 @@ class CodeGraph:
                 adjacency_matrix[edge.from_component.id][edge.to_component.id] = 1
                 adjacency_matrix[edge.to_component.id][edge.from_component.id] = 1
             except IndexError:
-                print(f"Index out of range for edge {edge} with from_component ID {edge.from_component.id} and to_component ID {edge.to_component.id}")
+                print(
+                    f"Index out of range for edge {edge} with from_component ID {edge.from_component.id} and to_component ID {edge.to_component.id}")
         return adjacency_matrix
-  
+
     def find_connected_nodes(self, node: Component) -> 'list[Component]':
         """ Finds all nodes connected to a given node
         """
@@ -234,7 +253,7 @@ class CodeGraph:
             elif edge.to_component == node:
                 connected_nodes.append(edge.from_component)
         return list(set(connected_nodes))
-  
+
     def _dfs_build_deps(self, node: ast.AST, parent: Component = None) -> None:
         """ Recursively builds the dependencies of a component node
         """
@@ -255,26 +274,28 @@ class CodeGraph:
                 self.add_edge(ComponentEdge(parent, current_component))
             parent = current_component
         elif isinstance(node, ast.FunctionDef):
-            current_component = Function(node.name, parent, [], ast.unparse(node))
+            current_component = Function(
+                node.name, parent, [], ast.unparse(node))
             self.codebase.funcDictionary[node.name] = current_component
             self.add_node(current_component)
             if parent:
                 self.add_edge(ComponentEdge(parent, current_component))
             parent = current_component
-    
+
         for child in ast.iter_child_nodes(node):
             self._dfs_build_deps(child, parent)
-   
+
     def populate_graph(self) -> None:
         """ Populates the graph with component nodes and edges representing
         the files, classes, and functions in the codebase and their dependencies
         """
-        print(f"Populating graph for {self.codebase.name}")
+        if self.debug:
+            print(f"Populating graph for {self.codebase.name}")
         for file in self.codebase.get_files():
             self.add_node(file)
             tree = ast.parse(file.raw)
             self._dfs_build_deps(tree, file)
-   
+
     def _dfs_build_func_calls(self, node: ast.AST, parent: Component = None) -> None:
         """ Recursively builds the function call edges of the graph
         """
@@ -289,17 +310,19 @@ class CodeGraph:
             if self.codebase.funcDictionary.__contains__(funcname):
                 dependency = self.codebase.funcDictionary[funcname]
                 self.add_edge(ComponentEdge(parent, dependency))
-    
+
         for child in ast.iter_child_nodes(node):
             self._dfs_build_func_calls(child, parent)
-   
+
     def populate_func_call_edges(self) -> None:
         """ Populates the graph with edges representing function calls
         """
         for file in self.codebase.get_files():
+            if self.debug:
+                print(f"Populating function call edges for {file.name}")
             tree = ast.parse(file.raw)
             self._dfs_build_func_calls(tree)
-   
+
     def delete_small_nodes(self, threshold: int = 300) -> None:
         """ Deletes nodes with raw code less than threshold
         """
@@ -309,7 +332,7 @@ class CodeGraph:
                 for edge in self.edges:
                     if edge.from_component == node or edge.to_component == node:
                         self.edges.remove(edge)
-      
+
     def remove_large_nodes(self, threshold: int = 1800) -> None:
         """ Removes nodes with raw code more than threshold
         """
@@ -319,9 +342,10 @@ class CodeGraph:
                 # Connect connected_nodes to each other
                 for i in range(len(connected_nodes)):
                     for j in range(i+1, len(connected_nodes)):
-                        self.add_edge(ComponentEdge(connected_nodes[i], connected_nodes[j]))
+                        self.add_edge(ComponentEdge(
+                            connected_nodes[i], connected_nodes[j]))
                 self.nodes.remove(node)
-    
+
     def remove_duplicate_edges(self) -> None:
         """ Removes duplicate edges from the graph
         """
@@ -330,7 +354,7 @@ class CodeGraph:
             if edge not in unique_edges and ComponentEdge(edge.to_component, edge.from_component) not in unique_edges:
                 unique_edges.add(edge)
         self.edges = list(unique_edges)
-        
+
     def create_id_to_raw(self) -> 'dict[int, str]':
         """ Creates a dictionary mapping node ids to their raw code
         """
@@ -345,90 +369,109 @@ class CodeGraph:
         """
         # If the json file already exists, overwrite it
         if os.path.exists(json_file_path):
-          os.remove(json_file_path)
+            os.remove(json_file_path)
         # Save examples to a JSON
         with open(json_file_path, 'w') as f:
             # Get id to raw dict
-            #this should update the correct node values from creare_id_to_raw
+            # this should update the correct node values from creare_id_to_raw
             id_to_raw = self.create_id_to_raw()
             json.dump(id_to_raw, f, indent=4, sort_keys=True)
-   
-    def create_index_to_json_dict(self) -> 'tuple[dict[int, int], dict[int, int]]':     
+
+    def create_index_to_json_dict(self) -> 'tuple[dict[int, int], dict[int, int]]':
         """ Creates a dictionary mapping node ids to their index
         """
-        id_to_json = {} # Index to JSON Data (node id)
-        json_to_id = {} # JSON Data (node id) to Index
+        id_to_json = {}  # Index to JSON Data (node id)
+        json_to_id = {}  # JSON Data (node id) to Index
         for i, node in enumerate(self.nodes):
             id_to_json[i] = node.id
             json_to_id[node.id] = i
         return (id_to_json, json_to_id)
-   
+
     def delete_edges_to_non_existent_nodes(self) -> None:
-      """ Deletes edges to non-existent nodes
-      """
-      true_length = len(self.nodes)
-      for edge in self.edges:
-        if edge.from_component != None and edge.to_component != None:
-          if edge.from_component.id >= true_length or edge.to_component.id >= true_length:
-            self.edges.remove(edge)
+        """ Deletes edges to non-existent nodes
+        """
+        true_length = len(self.nodes)
+        for edge in self.edges:
+            if edge.from_component != None and edge.to_component != None:
+                if edge.from_component.id >= true_length or edge.to_component.id >= true_length:
+                    self.edges.remove(edge)
 
     def reindex_nodes(self):
-      new_id = 0
-      for node in self.nodes:
-        node.id = new_id
-        new_id += 1
+        if self.debug:
+            print("Reindexing nodes")
+        new_id = 0
+        for node in self.nodes:
+            if self.debug:
+                print(f"Reindexing node {node.id} to {new_id}")
+            node.id = new_id
+            new_id += 1
 
     def print_nodes(self):
-      print('these are a list of all the nodes', self.nodes)
-      # for edge in self.edges:
-      # 	print('these are all the edges', edge)
+        print('these are a list of all the nodes', self.nodes)
+        # for edge in self.edges:
+        # 	print('these are all the edges', edge)
+
     def print_edges(self):
-      print('this is a list of all the edges', self.edges)
+        print('this is a list of all the edges', self.edges)
 
 
 class VisualCodeGraph():
-	""" Class for generating visualizations of the code graph
-	"""
-	
-	def __init__(self, codegraph: CodeGraph) -> None:
-		self.codegraph = codegraph
-  
-	def create_graph_dict(self) -> 'dict[File, list[dict[Class, list[Function]]]]':
-		""" Creates a dictionary representation of the graph
-				where keys are files and values are a list of dictionaries
-				representing the classes and their functions
+    """ Class for generating visualizations of the code graph
+    """
 
-				Example Output:
-				{
-						'file1.py': [
-								{
-										'Class1': ['Function1', 'Function2']
-								},
-								{
-										'Class2': ['Function3', 'Function4']
-								}
-						]
-				}
-		Returns:
-				dict[File, list[dict[Class, list[Function]]]]: The dictionary representation of the graph
-		"""
-		graph_dict = {}
-		nodes = self.codegraph.nodes
-		for node in nodes:
-			if node.is_file():
-				file = node
-				graph_dict[file] = []
-				# Get the connected nodes
-				connected_nodes = self.codegraph.find_connected_nodes(file)
-				for connected_node in connected_nodes:
-					if connected_node.is_class():
-						class_dict = {}
-						class_dict[connected_node] = []
-						# Get the connected nodes
-						connected_nodes2 = self.codegraph.find_connected_nodes(connected_node)
-						for connected_node2 in connected_nodes2:
-							if connected_node2.is_function():
-								class_dict[connected_node].append(connected_node2)
-						graph_dict[file].append(class_dict)
-		return graph_dict
+    def __init__(self, codegraph: CodeGraph) -> None:
+        self.codegraph = codegraph
+        self.graph_dict = self._create_graph_dict()
 
+    def get_graph_dict(self):
+        if self.graph_dict:
+            return self.graph_dict
+        else:
+            self.graph_dict = self._create_graph_dict()
+        return self.graph_dict
+
+    def _create_graph_dict(self) -> 'dict[File, list[dict[Class, list[Function]]]]':
+        """ Creates a dictionary representation of the graph
+                        where keys are files and values are a list of dictionaries
+                        representing the classes and their functions
+
+                        Example Output:
+                        {
+                                        'file1.py': [
+                                                        {
+                                                                        'Class1': ['Function1', 'Function2']
+                                                        },
+                                                        {
+                                                                        'Class2': ['Function3', 'Function4']
+                                                        }
+                                        ]
+                        }
+        Returns:
+                        dict[File, list[dict[Class, list[Function]]]]: The dictionary representation of the graph
+        """
+        graph_dict = {}
+        nodes = self.codegraph.nodes
+        for node in nodes:
+            if node.is_file():
+                print(f"File: {node.name}")
+                file = node
+                graph_dict[file] = []
+                # Get the connected nodes
+                connected_nodes = self.codegraph.find_connected_nodes(file)
+                for connected_node in connected_nodes:
+                    if connected_node.is_class():
+                        print(f"\tClass: {connected_node.name}")
+                        class_dict = {}
+                        class_dict[connected_node] = []
+                        # Get the connected nodes
+                        connected_nodes2 = self.codegraph.find_connected_nodes(
+                            connected_node)
+                        for connected_node2 in connected_nodes2:
+                            if connected_node2 == None:
+                                continue
+                            elif connected_node2.is_function():
+                                print(f"\t\tFunction: {connected_node2.name}")
+                                class_dict[connected_node].append(
+                                    connected_node2)
+                        graph_dict[file].append(class_dict)
+        return graph_dict
--- visualization.py
+++ visualization.py
@@ -1,7 +1,8 @@
 import plotly.express as px
 from napkin_graph import CodeBase, CodeGraph, VisualCodeGraph
 
-graphcastCodeBase = CodeBase("GraphCast", "graphcast", "https://github.com/google-deepmind/graphcast.git", skipCloning=True)
+graphcastCodeBase = CodeBase(
+    "GraphCast", "graphcast", "https://github.com/google-deepmind/graphcast.git", skipCloning=True)
 graphcastGraph = CodeGraph(graphcastCodeBase)
 graphcastGraph.populate_graph()
 # graphcastGraph.delete_small_nodes()
@@ -9,17 +10,19 @@ graphcastGraph.populate_func_call_edges()
 # graphcastGraph.remove_large_nodes()
 graphcastGraph.reindex_nodes()
 visualGraphCastCodeGraph = VisualCodeGraph(graphcastGraph)
-graph_dict = visualGraphCastCodeGraph.create_graph_dict()
+graph_dict = visualGraphCastCodeGraph.get_graph_dict()
 
+print(f"Graph Dictionary:\n{graph_dict}")
 
-#This is an example graph_dict for testing puproses, replace with visual_cg.get_graph_dict()
-#Make sure that there are no repeated names for files, classes, functions, and raw text
+# This is an example graph_dict for testing puproses, replace with visual_cg.get_graph_dict()
+# Make sure that there are no repeated names for files, classes, functions, and raw text
 
-graph_dict= {
+graph_dict = {
     'file1.py': [{'Class1': [{'Function1': "def raw_text"}, {'Function2': "def raw_text2"}]}],
     'file2.py': [{'Class3': [{'Function3': "def raw_text3"}, {'Function4': "def raw_text4"}, {'Function5': "def raw_text5"}]}],
     'file3.py': [{'Class4': [{'Function6': "def raw_text6"}, {'Function7': "def raw_text7"}, {'Function8': "def raw_text8"}]}]
 }
+
 
 class Visualization_Graph():
 
@@ -28,7 +31,6 @@ class Visualization_Graph():
         self.parents_list = []
         self.graph_dict = graph_dict
 
-    
     def generate_lists(self):
         ''' Populate the names_list and parents_list attributes based on the graph_dict.
             Iterates through the graph_dict, extracting file names, class names, function names, 
@@ -46,16 +48,15 @@ class Visualization_Graph():
                     self.parents_list.append(file)
                     # print(class_name)
                     for functions_dict in functions:
-                            # print(functions_dict)
-                            # print(functions)
-                            for functions_names, raw_texts in functions_dict.items():
-                                # print(functions_names)
-                                # print(raw_texts)
-                                self.names_list.append(functions_names)
-                                self.parents_list.append(class_name)
-                                self.names_list.append(raw_texts)
-                                self.parents_list.append(functions_names)
-
+                        # print(functions_dict)
+                        # print(functions)
+                        for functions_names, raw_texts in functions_dict.items():
+                            # print(functions_names)
+                            # print(raw_texts)
+                            self.names_list.append(functions_names)
+                            self.parents_list.append(class_name)
+                            self.names_list.append(raw_texts)
+                            self.parents_list.append(functions_names)
 
     def build_graph(self):
         '''
@@ -63,12 +64,13 @@ class Visualization_Graph():
         '''
         fig = px.treemap(
 
-            names= self.names_list,
+            names=self.names_list,
             parents=self.parents_list
         )
         fig.update_traces(root_color="lightgrey")
         fig.update_layout(margin=dict(t=50, l=25, r=25, b=25))
         fig.show()
+
 
 visualization_graph = Visualization_Graph()
 visualization_graph.generate_lists()
@@ -77,8 +79,7 @@ visualization_graph.generate_lists()
 # print(visualization_graph.parents_list)
 
 
-
-#Exampple Treemap for testing purposes to help build children and parents list
+# Exampple Treemap for testing purposes to help build children and parents list
 # fig = px.treemap(
 
 
@@ -90,37 +91,28 @@ visualization_graph.generate_lists()
 # fig.update_layout(margin=dict(t=50, l=25, r=25, b=25))
 # fig.show()
 
+# names=  ['file1.py', 'Class1', 'Function1', 'Function2', 'def raw_text1', 'def raw_text2'],
+# parents=['', 'file1.py', 'Class1', 'Class1', 'Function1', 'Function2']
 
+# names=  ['file1.py', 'Class1', 'Function1', 'Function2', 'Class2', 'Function3', 'Function4', 'file2.py', 'Class2.1', 'Function5', 'Function6', 'Class2.2', 'Function7', 'Function8', 'file3.py', 'Class3.1', 'Function9', 'Function10', 'Class3.2', 'Function11', 'Function12']
+# parents=['', 'file1.py', 'Class1', 'Class1', 'file1.py', 'Class2', 'Class2', '', 'file2.py', 'Class2.1', 'Class2.1', 'file2.py', 'Class2.2', 'Class2.2', '', 'file3.py', 'Class3.1', 'Class3.1', 'file3.py', 'Class3.2', 'Class3.2']
 
-    # names=  ['file1.py', 'Class1', 'Function1', 'Function2', 'def raw_text1', 'def raw_text2'],
-    # parents=['', 'file1.py', 'Class1', 'Class1', 'Function1', 'Function2']
-                    
-    # names=  ['file1.py', 'Class1', 'Function1', 'Function2', 'Class2', 'Function3', 'Function4', 'file2.py', 'Class2.1', 'Function5', 'Function6', 'Class2.2', 'Function7', 'Function8', 'file3.py', 'Class3.1', 'Function9', 'Function10', 'Class3.2', 'Function11', 'Function12']
-    # parents=['', 'file1.py', 'Class1', 'Class1', 'file1.py', 'Class2', 'Class2', '', 'file2.py', 'Class2.1', 'Class2.1', 'file2.py', 'Class2.2', 'Class2.2', '', 'file3.py', 'Class3.1', 'Class3.1', 'file3.py', 'Class3.2', 'Class3.2']
-
-
-
-    #This version shows without the raw text included 
-    # def generate_lists(self):
-    #     ''' Populate the names_list and parents_list attributes based on the graph_dict.
-    #         Iterates through the graph_dict, extracting file names, class names, and function names
-    #         to populate names_list and their corresponding parent relationships in parents_list.
-    #     '''
-    #     for file, classes in self.graph_dict.items():
-    #         self.names_list.append(file)
-    #         self.parents_list.append('')
-    #         for classes_dict in classes:
-    #             for class_name, functions in classes_dict.items():
-    #                 self.names_list.append(class_name)
-    #                 self.parents_list.append(file)
-    #                 # print(class_name)
-    #                 for function in functions:
-    #                     self.names_list.append(function)
-    #                     self.parents_list.append(class_name)
-    #                     # print(function)
-    #                     pass
-
-
-
-
-        
+# This version shows without the raw text included
+# def generate_lists(self):
+#     ''' Populate the names_list and parents_list attributes based on the graph_dict.
+#         Iterates through the graph_dict, extracting file names, class names, and function names
+#         to populate names_list and their corresponding parent relationships in parents_list.
+#     '''
+#     for file, classes in self.graph_dict.items():
+#         self.names_list.append(file)
+#         self.parents_list.append('')
+#         for classes_dict in classes:
+#             for class_name, functions in classes_dict.items():
+#                 self.names_list.append(class_name)
+#                 self.parents_list.append(file)
+#                 # print(class_name)
+#                 for function in functions:
+#                     self.names_list.append(function)
+#                     self.parents_list.append(class_name)
+#                     # print(function)
+#                     pass
