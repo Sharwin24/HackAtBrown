645a1f962f3f8426aa4f3ff9fa24b1e9d304fe36
CharlesJoseph2003 <105239968+CharlesJoseph2003@users.noreply.github.com>
2024-04-05 12:19:32 -0400

    Adding App.py

--- App.py
+++ App.py
@@ -14,3 +14,8 @@ Visualization_callback(app)
 
 if __name__ == '__main__':
     app.run_server(debug=False)
+
+#https://github.com/google-research/bert.git
+#https://github.com/Sharwin24/IMU-RobotArm-Control.git
+#https://github.com/google-deepmind/graphcast.git
+
